bf2c524eb9772f8373e9e9c26d16ed68988c38fc
kobayashi1027 <koba1027yashi@gmail.com>
2014-11-28 15:51:56 +0900

    Make mail data gettable

--- lib/perican/resource/mail.rb
+++ lib/perican/resource/mail.rb
@@ -1,1 +1,27 @@
-# fill later
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+ENV["BUNDLE_GEMFILE"] = File.expand_path("../../../../Gemfile", __FILE__)
+
+require 'rubygems'
+require 'bundler/setup'
+require 'mail'
+require 'net/imap'
+require 'yaml'
+
+yml_data = YAML.load_file('./setting.yml')
+imap = Net::IMAP.new('imap.gmail.com', 993, true)
+imap.login(yml_data["address"], yml_data["password"])
+
+imap.select('INBOX')
+ids = imap.search(["ALL"])
+imap.fetch(ids, "RFC822").each do |mail|
+  m = Mail.new(mail.attr["RFC822"])
+
+  puts "Date: " + m.date.to_s
+  puts "To: " + m.to.to_s
+  puts "From: " + m.from.to_s
+  puts "Subject: " + m.subject.to_s
+  puts "\n"
+
+end
